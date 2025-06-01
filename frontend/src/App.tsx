798978871d048d326f56de93dc52b177548ed6a4
dev <thakurdev2672@gmail.com>
2025-06-01 20:55:21 +0530

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='bg-green-500 mx-auto text-white'>
+        <h1>hello this is new type of vite apppppppp</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
