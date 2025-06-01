f89a6a084a024988069326bbc54c9077b61fe338
dev <thakurdev2672@gmail.com>
2025-06-02 03:35:10 +0530

    Header Footer and Hero added in frntend

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+    return (
+      <div className="bg-gray-700 pb-16 ">
+        <div className="container mx-auto flex flex-col gap-2">
+          <h1 className="text-5xl text-white font-bold">Find your next spot</h1>
+          <p className="text-2xl text-white">
+            Search your prefered charger station.
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Hero;
