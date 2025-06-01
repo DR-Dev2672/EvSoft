4e4b54bb94c9140a63b37a0feca63acf2631097f
dev <thakurdev2672@gmail.com>
2025-06-01 23:58:55 +0530

    station model added

---
+++ backend/src/models/station.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { StationType } from "../shared/types";
+
+const stationSchema =new mongoose.Schema<StationType>({
+ name:{
+    type:String,
+    required:true,
+ },
+ location:{
+    latitude:{
+        type:Number,
+        required:true,
+    },
+    longitude:{
+        type:Number,
+        required:true,
+    }
+ },
+ status:{
+    type:String,
+    required:true,
+    enum:["Active","Inactive"]
+ },
+ powerOutput:{
+    type:Number,
+    required:true,
+ },
+    connectorType:{
+        type:String,
+        required:true,
+        enum:["Type1","Type2","CCS","CHAdeMO"]
+    }   
+})
