876ea6fd26eb12944299c0972d5a718f7f70fb32
dev <thakurdev2672@gmail.com>
2025-06-02 23:16:25 +0530

    basic setup for userRoute in index.js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,19 +2,39 @@ import express,{Request,Response} from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 const app=express();
-const PORT =3000;
+const PORT =8000;
 import "dotenv/config";
 import path from "path";
+import userRoutes from "./routes/user.route";
+import e from "express";
+
 
 
 //database connection
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 console.log("Database connected successfully");
+
+
+
+//uses
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended:true}))
+
 app.get("/",(req:Request,res:Response)=>{
     res.status(200).json({
         message:"hiii from server side"
     })
 })
+
+//routes
+
+// app.use("/api/users",userRoutes);
+
+
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
