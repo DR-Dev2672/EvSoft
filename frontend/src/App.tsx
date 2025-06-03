7e638b39d8ebc620efa37edc2a392bf5d1bf93e4
dev <thakurdev2672@gmail.com>
2025-06-04 03:04:46 +0530

    route added in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import Layout from "./layout/Layout"
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Register from "./pages/Register";
+import Search from "./pages/Search";
+import AddStation from "./pages/AddStation";
+import Details from "./pages/Details";
+import MyStations from "./pages/MyStations";
 
 
 function App() {
@@ -10,9 +17,58 @@ function App() {
     <>
       <BrowserRouter>
         <Routes> 
-          <Route path="/" element={<Layout/>}/> 
+          <Route path="/"
+           element={
+            <Layout>
+              <Home />
+            </Layout>
+            }
+          /> 
+          <Route path="/register"
+           element={
+            <Layout>
+              <Register />
+            </Layout>
+            }
+          /> 
+          <Route path="/sign-in"
+           element={
+            <Layout>
+              <SignIn />
+            </Layout>
+            }
+          /> 
+          <Route path="/search"
+           element={
+            <Layout>
+              <Search />
+            </Layout>
+            }
+          /> 
+          <Route path="/add-station"
+           element={
+            <Layout>
+              <AddStation />
+            </Layout>
+            }
+          /> 
+          <Route path="/details/:stationId"
+           element={
+            <Layout>
+              <Details/>
+            </Layout>
+            }
+          /> 
+          <Route path="/my-stations"
+           element={
+            <Layout>
+              <MyStations />
+            </Layout>
+            }
+          /> 
            
         </Routes>
+        
         </BrowserRouter>
     </>
   )
