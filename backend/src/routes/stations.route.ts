8e70ffa5e0c3f994c19de9e28521cfedcf65fcac
dev <thakurdev2672@gmail.com>
2025-06-05 22:26:01 +0530

    route for station details added

--- backend/src/routes/stations.route.ts
+++ backend/src/routes/stations.route.ts
@@ -3,6 +3,7 @@ import express ,{Request,Response} from "express";
 const app=express();
 const router=express.Router();
 import Station from "../models/station.model"
+import { param, validationResult } from "express-validator";
 
 
 
@@ -15,5 +16,26 @@ router.get("/", async (req: Request, res: Response) => {
     res.status(500).json({ message: "Error fetching stations" });
   }
 });
+
+router.get(
+  "/:id",
+  [param("id").notEmpty().withMessage("Station ID is required")],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const id = req.params.id.toString();
+
+    try {
+      const station = await Station.findById(id);
+      res.json(station);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error fetching station" });
+    }
+  }
+);
 
 export default router;
