deffb26c68a7076ac6ec90d89c8d4fd94bcadeb5
dev <thakurdev2672@gmail.com>
2025-06-06 20:45:18 +0530

    Google Map with marker is added

--- frontend/src/pages/Map.tsx
+++ frontend/src/pages/Map.tsx
@@ -1,25 +1,38 @@
 import React from 'react'
+import * as apiClient from '../api-client'
+import { useQuery } from 'react-query'
+
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
+const API_KEY=import.meta.env.VITE_API_G_P 
 
 const containerStyle = {
-  width: '400px',
+  width:' 900px',
   height: '400px',
 }
 
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-}
+
+
 
 function MyComponent() {
+   const { data: stations } = useQuery("fetchQuery", () =>
+      apiClient.fetchStations()
+    );
+
+    const points=stations?.map((station) => ({
+      lat: station.location.latitude,
+      lng: station.location.longitude
+    })) || [];
+    console.log("points", points);
+    const center=points[5];
+
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: 'YOUR_API_KEY',
+    googleMapsApiKey:API_KEY,
   })
 
-  const [map, setMap] = React.useState(null)
+  const [map, setMap] = React.useState<any>(null)
 
-  const onLoad = React.useCallback(function callback(map) {
+  const onLoad = React.useCallback(function callback(map:any) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center)
     map.fitBounds(bounds)
@@ -27,7 +40,7 @@ function MyComponent() {
     setMap(map)
   }, [])
 
-  const onUnmount = React.useCallback(function callback(map) {
+  const onUnmount = React.useCallback(function callback(map:any) {
     setMap(null)
   }, [])
 
@@ -40,12 +53,15 @@ function MyComponent() {
       onUnmount={onUnmount}
     >
       {/* Child components, such as markers, info windows, etc. */
+
+       
+        
       
-      <Marker position={center}></Marker>
-      }
-      <>
+      // <Marker position={center}></Marker>
+      points.map((point, index) => (
+        <Marker key={index} position={point} />
+      ))}
       
-      </>
     </GoogleMap>
   ) : (
     <></>
