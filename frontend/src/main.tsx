c255263d91e0fc1c18392840225b138d6458267a
dev <thakurdev2672@gmail.com>
2025-06-06 20:48:08 +0530

    app file wrapping

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from "react-query";
+import { AppContextProvider } from './context/AppContext.tsx';
 
 
 const queryClient = new QueryClient({
@@ -16,7 +17,9 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+    <AppContextProvider>
     <App />
+    </AppContextProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
