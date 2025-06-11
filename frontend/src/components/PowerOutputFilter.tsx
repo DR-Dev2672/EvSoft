ee266195c6a383a364fc5b92afc10f328dae0e9e
dev <thakurdev2672@gmail.com>
2025-06-11 23:54:56 +0530

    Power output filter thinking

---
+++ frontend/src/components/PowerOutputFilter.tsx
@@ -0,0 +1,20 @@
+
+
+function PowerOutputFilter() {
+  return (
+    
+
+    <div>
+        <h4>Power Output</h4>
+        <label className="">
+            <input
+             type="number"
+             value={""}
+             onChange={() => {}}
+             />
+        </label>
+    </div>
+  )
+}
+
+export default PowerOutputFilter
