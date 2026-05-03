6a4a2a9e497e4092485d1320cc6d30bf60f39c2e
Dev Raj Dev <thakurdev2672@gmail.com>
2026-05-03 21:26:40 +0530

    shacn comp added with config.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+  },
 })
